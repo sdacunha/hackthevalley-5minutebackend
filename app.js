a8d4644e2ff43b3cf77c1c05d4ad17b1ea0e8097
Shadwell Da Cunha <shadwell@wifihost-143-25.wireless.utsc.utoronto.ca>
2017-01-08 07:29:58 -0500

    Added logging

--- app.js
+++ app.js
@@ -11,15 +11,18 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
     socket.on('beacon-ping', function(msg){
         phones[msg].emit("ping");
+        console.log("Beacon sent purchase ping to " + msg);
     });
     socket.on('beacon-purchased', function(msg){
         phones[msg].emit("purchased");
+        console.log("Beacon sent purchased ping to " + msg);
     });
     socket.on('phone', function(msg){
         phones[msg] = socket
+        console.log("Phone connected: " + msg);
     });
 });
 
 http.listen(80, "0.0.0.0", function(){
-    console.log('listening on *:3000');
+    console.log('listening on *:80');
 });
